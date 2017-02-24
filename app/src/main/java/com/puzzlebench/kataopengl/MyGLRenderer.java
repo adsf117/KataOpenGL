f258bcf6e6af3981111f60ad3ad09a210177d141
Andres David <ads.f117@gmail.com>
2017-02-24 12:34:34 -0500

    Draw a Triangle .. done !

--- app/src/main/java/com/puzzlebench/kataopengl/MyGLRenderer.java
+++ app/src/main/java/com/puzzlebench/kataopengl/MyGLRenderer.java
@@ -11,12 +11,12 @@ import javax.microedition.khronos.opengles.GL10;
  */
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
-
+    private Triangle triangle;
     //Called once to set up the view's OpenGL ES environment.
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(0.0f, 0.0f, 51.0f, 1.0f); // Set the background frame color
-
+        triangle = new Triangle();
     }
     //Called for each redraw of the view.
     @Override
@@ -28,5 +28,18 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        triangle.draw();
+    }
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
     }
 }
