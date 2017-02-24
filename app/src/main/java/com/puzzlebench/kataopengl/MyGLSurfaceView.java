43cf9c50f199f3dec7f14746b53901734666e9e8
Andres David <ads.f117@gmail.com>
2017-02-24 14:00:27 -0500

    add automatically rotated

--- app/src/main/java/com/puzzlebench/kataopengl/MyGLSurfaceView.java
+++ app/src/main/java/com/puzzlebench/kataopengl/MyGLSurfaceView.java
@@ -21,7 +21,7 @@ import android.opengl.GLSurfaceView;
 
         // Set the Renderer for drawing on the GLSurfaceView
         setRenderer(mRenderer);
-        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 
     }
 }
